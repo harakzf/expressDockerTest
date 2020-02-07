7d442aa85de3ede27dd9adb5774439687b646f1c
Kazufumi Hara <you@example.com>
2020-02-07 15:06:30 +0900

    change20200207

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+
+  const pool = pg.Pool({
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    host: '172.17.0.2',
+    // host: 'localhost',
+    port: 5432
+  });
+
+  const query = { text: 'SELECT * FROM cars' };
+
+  pool.connect(async (err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const resObj = await client.query(query);
+      console.log(resObj.rows);
+      console.log('DBアクセス成功。');
+      res.render('index', { title: 'Express', datas: resObj.rows[0].name });
+    }
+  })
+
+
+
 });
 
 module.exports = router;
